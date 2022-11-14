200e8dca035fda137e2d78758efc278df453f2af
Juanma Cozzuol <juanmacozzuol@gmail.com>
2022-11-14 13:17:09 -0300

    safe credentials

--- config.js
+++ config.js
@@ -3,5 +3,9 @@ dotenv.config()
 
 module.exports={
     DB_PASSWORD: process.env.DB_PASSWORD,
-    DATASOURCE: process.env.DATASOURCE
+    DATASOURCE: process.env.DATASOURCE,
+    PROJECT_ID :process.env.PROJECT_ID,
+    PRIVATE_KEY_ID : process.env.PRIVATE_KEY_ID,
+    PRIVATE_KEY : process.env.PRIVATE_KEY,
+    CLIENTE_EMAIL : process.env.CLIENTE_EMAIL
 }
--- db/db.config.js
+++ db/db.config.js
@@ -1,7 +1,10 @@
 const envConfig = require("../config")
 const firebaseConfig = require('./firebase/firebase.config.json')
 
-
+firebaseConfig.project_id = envConfig.PROJECT_ID
+firebaseConfig.private_key_id = envConfig.PRIVATE_KEY_ID
+firebaseConfig.private_key = envConfig.PRIVATE_KEY
+firebaseConfig.client_email = envConfig.CLIENTE_EMAIL
 module.exports ={
     mongodb:{
         uri:`mongodb+srv://jmfcozzuol:${envConfig.DB_PASSWORD}@coderhousefinalproyect.3cwsvlv.mongodb.net/proyecto_final?retryWrites=true&w=majority`
